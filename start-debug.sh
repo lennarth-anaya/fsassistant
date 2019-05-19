c12b531b09762bd102a9b4e2c3cedcffb1be0f5a
lennarth-anaya <lennarth.anaya@gmail.com>
2019-05-18 19:48:24 -0500

    Setup: Easing startup scripts that can easily be ran with sudo java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=8000,suspend=n  -jar ./target/fs-assistant-0.0.1-SNAPSHOT.jar sudo java -jar ./target/fs-assistant-0.0.1-SNAPSHOT.jar

--- start-debug.sh
+++ start-debug.sh
@@ -1,3 +1,1 @@
-# it might be used Spring boot maven plugin (or gradle in other projects)
-# but it's nicer to use the most generic approach despite the build tool
 sudo java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=8000,suspend=n  -jar ./target/fs-assistant-0.0.1-SNAPSHOT.jar 
--- start.sh
+++ start.sh
@@ -1,3 +1,1 @@
-# it might be used Spring boot maven plugin (or gradle in other projects)
-# but it's nicer to use the most generic approach despite the build tool
 sudo java -jar ./target/fs-assistant-0.0.1-SNAPSHOT.jar 
